d432c0daa0cd2b5d7230dae737eddd38f574493e
OlujimiA <olujimiebitimi@gmail.com>
2024-12-24 10:48:58 +0100

    Made index and login file

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<?php
-    echo "This is an Event Booking System";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Event Booking System</title>
+</head>
+<body>
+    <?php
+        echo "This is an Event Booking System";
+    ?> 
+    <p>Already have an account? Login <a href="login.php">here</a></p>
+</body>
+</html>
+
